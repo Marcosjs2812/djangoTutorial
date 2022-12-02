0ee62eecc0c644281f4b095af5d4bc9154f1fef0
Marcosjs2812 <mjuarezs1101@alumno.ipn.mx>
2022-12-02 13:20:00 -0600

    Added tables project and task

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model): # This class is like a table in a database
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # This is a foreign key, which is a reference to another table
