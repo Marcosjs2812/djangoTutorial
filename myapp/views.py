edce60258a29ff591f3848097752fd1daa5ab4fa
Marcosjs2812 <mjuarezs1101@alumno.ipn.mx>
2022-12-02 23:10:43 -0600

    Paths that recieve values as argument to interact with the database in the views

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello Wordl!</h1>")
+def index(request):
+    return HttpResponse("Index Page")
+
+def hello(request, username):
+    return HttpResponse(f"<h1>Hello {username}!</h1>")
+
 def about(request):
     return HttpResponse("About Page")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def task(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse(f"task {task.title} has id {task.id}")
