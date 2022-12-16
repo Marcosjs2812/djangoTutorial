b0769ddc17a88cd56922258ea0b759422789a1c0
Marcosjs2812 <mjuarezs1101@alumno.ipn.mx>
2022-12-15 22:51:30 -0600

    Started using forms and methods GET and POST to interact with the backend.

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
 
@@ -26,13 +26,34 @@ def hello(request, username):
 def projects(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
+    return render(request, 'projects/projects.html', {
         'projects': projects
     })
 
 
 def tasks(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks/tasks.html', {
         'tasks': tasks
     })
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('/tasks/')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        Project.objects.create(name=request.POST['name'])
+        return redirect('/projects/')
